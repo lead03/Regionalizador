eaf5761e4d15e47cf7f48e40ab43991700e56287
Leandro Duhart <leandro.duhart@gmail.com>
2024-05-12 18:46:01 -0300

    Creado general

---
+++ src/app/components/Grafo.java
@@ -0,0 +1,37 @@
+package app.components;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Grafo {
+	private boolean[][]A;
+	
+	public Grafo(int vertices) {
+		A = new boolean[vertices][vertices];
+	}
+	
+	public void insertarArista(int i, int j) {
+		A[i][j] = true;
+		A[j][i] = true;
+	}
+	
+	public boolean existeArista (int vertice1, int vertice2) {
+		return A[vertice1][vertice2];
+	}
+	
+	private int tamano()
+	{
+		return A.length;
+	}
+
+	
+	public Set<Integer> obtenerVecinos (int indiceArista){
+		Set<Integer> ret = new HashSet<Integer>();
+		for(int j = 0; j < this.tamano(); ++j) if( indiceArista != j )
+		{
+			if( this.existeArista(indiceArista,j) )
+				ret.add(j);
+		}	
+		return ret;
+	}
+}
