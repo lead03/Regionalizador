30be2f627cc08d8369fbf533aa769b34b603b090
Leandro Duhart <leandro.duhart@gmail.com>
2024-05-12 19:14:40 -0300

    Creado de manejo de datos sin manejo de errores

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -1,5 +1,17 @@
 package app;
 
-public class Programa {
+import app.componentes.Grafo;
 
+
+public class Programa {
+	
+	private Grafo grafo;
+	
+	public void construirGrafo(int cantidadVertices) {
+		this.grafo = new Grafo(cantidadVertices);
+	}
+	
+	public void agregarArista (int vertice1, int vertice2) {
+		this.grafo.insertarArista(vertice1, vertice2);
+	}
 }
